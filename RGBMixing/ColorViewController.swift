59cf2d5215e41ff1e51c08aa5c4c7479d09b22b4
Elisei Mishchenko <esm.mail@icloud.com>
2021-12-25 19:39:15 +0300

    Work on logic.

--- RGBMixing/ColorViewController.swift
+++ RGBMixing/ColorViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ColorViewController: UIViewController {
+protocol ColorViewControllerDelegate {
+    func color(color: UIColor)
+}
 
+class ColorViewController: UIViewController, ColorViewControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? RGBMixingViewController else { return }
+        destination.delegate = self
+    }
+    
+    func color(color: UIColor) {
+        view.backgroundColor = color
+    }
+    
+    @IBAction func unwind(for seque: UIStoryboardSegue) {
+      
+    }
 
 }
+
 
--- RGBMixing/RGBMixingViewController.swift
+++ RGBMixing/RGBMixingViewController.swift
@@ -7,23 +7,112 @@
 
 import UIKit
 
+
 class RGBMixingViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    
+    weak var delegate: ColorViewController?
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
+        
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
     }
     
+     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: - IBAction
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider: setValue(for: redLabel)
+        case greenSlider: setValue(for: greenLabel)
+        default: setValue(for: blueLabel)
+        }
     }
-    */
-
+    
+    @IBAction func tap(_ sender: Any) {
+        redTextField.resignFirstResponder()
+        greenTextField.resignFirstResponder()
+        blueTextField.resignFirstResponder()
+    }
+    
+    @IBAction func changeColorInVC() {
+        delegate?.color(color: UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value)
+            , alpha: 1)
+        )
+    }
+    
+    
+    
+    //MARK: - Private Methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                label.text = string(from: redSlider)
+            case greenLabel:
+                label.text = string(from: greenSlider)
+            default:
+                label.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
+
+
+extension RGBMixingViewController: UITextFieldDelegate {
+     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+         if textField == redTextField {
+             greenTextField.becomeFirstResponder()
+         } else if textField == greenTextField {
+             blueTextField.becomeFirstResponder()
+         }
+        return true
+    }
+}
+
+
+
